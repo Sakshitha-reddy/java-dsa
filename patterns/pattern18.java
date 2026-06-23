b9c126bad58fd0c2ba785cef5fb6fe16d0e891b6
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-06-23 19:59:38 +0530

    pattern 18

---
+++ patterns/pattern18.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class pattern18 {
+	public static void main (String[] args) {
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <=n-i; j++) {
+                System.out.print( " ");
+            }
+            for (int j = 1; j <=i; j++) {
+                System.out.print(ch);
+                ch++;
+            }
+            for (int j = 1; j<=i-1; j++) {
+                ch--;
+                System.out.print((char)(ch-1));
+            }
+            System.out.println();
+ }
+		}
+    }
