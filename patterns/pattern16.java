8723ea6172241ccb58fc695f3540f30beb2f00f7
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-06-23 19:40:35 +0530

    pattern 16

---
+++ patterns/pattern16.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class pattern16 {
+	public static void main (String[] args) {
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+        
+        for(int i=0;i<n;i++){
+			for(char ch='A';ch<='A'+(n-i-1);ch++){
+                System.out.print(ch+" ");
+            }
+		System.out.println();
+		}
+		}
+    }
