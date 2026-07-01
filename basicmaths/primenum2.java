c2baf702873c3bbc069cf4880c31c982511c0eee
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-07-01 16:17:23 +0530

    prime num 2

---
+++ basicmaths/primenum2.java
@@ -0,0 +1,21 @@
+// this is a different logic to solve prime num problem
+
+
+
+
+// class Solution {
+//     public boolean isPrime(int n) {
+
+//         int count=0;
+//         for(int i=1;i*i<=n;i++){
+//             if(n%i==0){
+//                 count=count+1;
+//                 if((n/i)!=i){
+//                     count=count+1;
+//                 }
+//             }
+            
+//         }
+//         return count==2;
+//     }
+// }
